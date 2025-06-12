1526d174ea27ba16d57c035a03d26f8d10e8521d
Julio Toboso <JulioToboso@gmail.com>
2025-06-12 21:20:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,6 @@ import './components/task-modal.js';          // registers <task-modal>
 /* 2) Constants */
 const PRIORITIES = ['Alta', 'Media', 'Baja', 'Retraso', 'Completa'];
 
-const firebaseConfig = {
-  apiKey            : 'AIzaSyA-EY3-EISZdoThqVepYYA9rlCI30Qt8ZE',
-  authDomain        : 'taska-65c33.firebaseapp.com',
-  projectId         : 'taska-65c33',
-  storageBucket     : 'taska-65c33.appspot.com',
-  messagingSenderId : '287205600078',
-  appId             : '1:287205600078:web:25b211ff3764cbfe304c1f'
-  };
 
 
 const TaskModule = {
